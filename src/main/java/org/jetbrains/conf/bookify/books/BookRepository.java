38b21f2b5e99c62584238a4e03f8086f9973bb36
Anton Kozub <anton.kozub@jetbrains.com>
2025-11-12 15:35:19 +0100

    Removed aot

--- src/main/java/org/jetbrains/conf/bookify/books/BookRepository.java
+++ src/main/java/org/jetbrains/conf/bookify/books/BookRepository.java
@@ -2,6 +2,7 @@ package org.jetbrains.conf.bookify.books;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.relational.core.mapping.event.BeforeConvertCallback;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 interface BookRepository extends CrudRepository<Book, UUID> {
+    @Query("SELECT book.id AS id, book.isbn AS isbn, book.name AS name, book.available AS available FROM book WHERE book.name LIKE UPPER(:name)")
     List<Book> findByNameContainingIgnoreCase(String name);
 }
 
